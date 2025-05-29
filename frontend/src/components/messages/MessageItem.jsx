31bc4891367c2ccfc899cd5f67dab5afee9426c1
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-05-30 00:38:07 +0300

    Убрал лишнюю фильтрацию текста с вывода

--- frontend/src/components/messages/MessageItem.jsx
+++ frontend/src/components/messages/MessageItem.jsx
@@ -1,16 +1,7 @@
-import filterProfanity from '../../utils/profanityFilter'
-
 const MessageItem = ({ message }) => {
-  const safeBody = filterProfanity(String(message.body || message.text || ''))
-
   return (
     <div className="mb-2">
-      <b>
-        {message.username}
-        :
-        {' '}
-      </b>
-      <span>{safeBody}</span>
+      <b>{message.username}:</b> <span>{message.body || message.text || ''}</span>
     </div>
   )
 }
