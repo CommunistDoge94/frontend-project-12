def3d00da990410b2ea01e1ff48a076533364d90
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-05-30 15:17:58 +0300

    Реализация логики редиректа авторизованных пользоватей с /login и /signup на /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,8 @@ import Header from './components/Header.jsx'
 import SignupPage from './pages/SignupPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import ChatPage from './pages/ChatPage.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
+import PrivateRoute from './components/routes/PrivateRoute.jsx'
+import UnauthenticatedRoute from './components/routes/UnauthenticatedRoute.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 
 const App = () => {
@@ -16,15 +17,29 @@ const App = () => {
         <Header />
         <div className="flex-grow-1 overflow-hidden">
           <Routes>
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/signup"
+              element={
+                <UnauthenticatedRoute>
+                  <SignupPage />
+                </UnauthenticatedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <UnauthenticatedRoute>
+                  <LoginPage />
+                </UnauthenticatedRoute>
+              }
+            />
             <Route
               path="/"
-              element={(
+              element={
                 <PrivateRoute>
                   <ChatPage />
                 </PrivateRoute>
-              )}
+              }
             />
             <Route path="/404" element={<NotFoundPage />} />
             <Route path="*" element={<Navigate to="/404" replace />} />
